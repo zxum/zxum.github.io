435461c05b08da501ea19259f034603273276371
zxum <zion.um@outlook.com>
2021-05-05 09:56:17 -0500

    Add styling and content to About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,30 @@
 import React from 'react'
 
 
-function About() {
-
-  // Import images for use 
-  let requestImageFiles = require.context('../assets',true,/.jpg$|.gif$/) 
+function About(props) {
 
   return (
     <section id="about">
       <div id="about-box">
-        <img src={requestImageFiles("./me.jpg").default} alt=""/>
-        <div className="about-content">
-          <p className="subtitle-grey">Full Stack Engineer</p>
-          <h1>Hello, I'm Zion!</h1>
-          <p className="text light-grey">Ruby on Rails, ReactJS are my specialities! In my freetime I make art, hike, or rock climb!</p>
-          <p className="text" id="cta">Contact me to collaborate!</p>
-          <div className="link-bar">
-            <i class="fab fa-github-square"></i>
-            <i class="fab fa-linkedin"></i>
-            <i class="fas fa-envelope-square"></i>
-            <button>Resume</button>
+        <div id="top-bar">
+          <div className="circle" id="close-btn"></div>
+          <div className="circle" id="min-btn"></div>
+          <div className="circle" id="open-btn"></div>
+          <p>About</p>
+        </div>
+        <div id="content">
+          <img src={props.requestImageFiles("./me.jpg").default} alt=""/>
+          <div className="about-content">
+            <p id="job-title">Full Stack Engineer</p>
+            <h1 id="name">Hello, I'm Zion!</h1>
+            <p id="description">Ruby on Rails, ReactJS are my specialities! In my freetime I make art, hike, or rock climb!</p>
+            <p id="cta">Contact me to collaborate!</p>
+            <div id="link-bar">
+              <i className="fab fa-github-square github-icon"></i>
+              <i className="fab fa-linkedin linkedin-icon"></i>
+              <i className="fas fa-envelope-square email-icon"></i>
+              <button id="resume-button">Resume</button>
+            </div>
           </div>
         </div>
       </div>
